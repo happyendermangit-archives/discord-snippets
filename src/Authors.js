1caf3f2580fee2869ae7ad1b7fc2af286ef96689
= <=>
2024-02-04 22:36:52 +0000

    remove examples

--- src/Authors.js
+++ src/Authors.js
@@ -3,16 +3,6 @@ const Authors = {
         name:"Happy enderman",
         id:"1083437693347827764",
         pfp:"https://images-ext-1.discordapp.net/external/20woDoWVvRDuuYN3U220oo4COOEKKf_H0tOn5WV8luw/%3Fsize%3D512/https/cdn.discordapp.com/avatars/1083437693347827764/a66be3c66effa6b587df782a19e879f6.webp?format=webp&width=111&height=111"
-    },
-    ZYRENTH:{
-        name:"Zyrenth",
-        id:"509018277549309962",
-        pfp:"https://images-ext-1.discordapp.net/external/EZLXIBuZ_URx5DiRZ500viBUWE30Dypj05OSgmILnis/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/509018277549309962/2b9d4ad5a5a04662d1bd5f11c2ae8144.png?format=webp&quality=lossless&width=320&height=320"
-    },
-    TOLGCHU:{
-        name:"Tolgchu",
-        id:"329671025312923648",
-        pfp:"https://cdn.discordapp.com/avatars/329671025312923648/933f6137e08e3471d42e8bd4749d12a2.png?size=2048"
     }
 }
 
